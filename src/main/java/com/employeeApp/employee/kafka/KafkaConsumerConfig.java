94327ae40ea92ee8c9dce6f28a3a139f6d5cbc88
Erce Can Balcioglu <ercecanbalcioglu@gmail.com>
2023-08-14 01:36:56 +0200

    ADD: Kafka Producer and Consumer configs

--- src/main/java/com/employeeApp/employee/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/employeeApp/employee/kafka/KafkaConsumerConfig.java
@@ -1,7 +1,8 @@
-package com.employeeApp.employeeApi.kafka;
+package com.employeeApp.employee.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,11 +17,13 @@ import java.util.Map;
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public ConsumerFactory<String, EmployeeEvent> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092, localhost:39092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "employee-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
